73e7cb34e4c05b425bb662191c5cad1e3ef5f641
shady <saifulhaq.15@outlook.com>
2020-09-10 22:45:10 +0500

    initial commit

---
+++ lib/data/core/api_constants.dart
@@ -0,0 +1,7 @@
+class APIConstants {
+  APIConstants._();
+
+  static const BASE_URL = "https://api.themoviedb.org/3/";
+  static const API_KEY = "b730547051beed2688851a448ad59ac3";
+  static const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500/";
+}
---
+++ lib/domain/entities/movie_entity.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:equatable/equatable.dart';
+
+class MovieEntity extends Equatable {
+  final String posterPath;
+  final int id;
+  final String backdropPath;
+  final String title;
+  final num voteAverage;
+  final String releaseDate;
+  final String overview;
+
+  const MovieEntity({
+    @required this.posterPath,
+    @required this.id,
+    @required this.backdropPath,
+    @required this.releaseDate,
+    @required this.title,
+    @required this.voteAverage,
+    this.overview,
+  }) : assert(id != null, "MOvie id must not be null");
+
+  @override
+  List<Object> get props => [id, title];
+
+  @override
+  bool get stringify => true;
+}
